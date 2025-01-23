23c62d1fcd04f744983ef6e07db06fe2ecd67c19
Pratik Kamble <prak@ciklum.com>
2025-01-23 16:25:30 +0530

    Completed basic http server section

---
+++ http-server/http-server.js
@@ -0,0 +1,10 @@
+const http = require("http");
+const { handler, someOtherData } = require("./routes");
+
+console.log(someOtherData);
+
+const server = http.createServer(handler);
+
+server.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
---
+++ http-server/routes.js
@@ -0,0 +1,53 @@
+const fs = require("fs");
+
+const requestHandler = (req, res) => {
+  if (req.url == "/") {
+    res.setHeader("Content-type", "text/html");
+
+    res.write("<html>");
+    res.write("<body>");
+    res.write(
+      "<form action='/message' method='POST'><input name='message'/><button type='submit'>Submit</button></form>"
+    );
+    res.write("<p>This is a test</p>");
+    res.write("</body>");
+    res.write("</html>");
+    return res.end();
+  }
+  if (req.url == "/message") {
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      // fs.writeFileSync("message.txt", message); // Code line runs synchronously, So after this line code will not execute till this code executed completely
+      fs.writeFile("message.txt", message, (err) => {
+        // this runs async
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end();
+      });
+    });
+  }
+  res.setHeader("Content-type", "text/html");
+  res.write("<html>");
+  res.write("<body>");
+  res.write("<h1>Hello World</h1>");
+  res.write("<p>This is a test</p>");
+  res.write("</body>");
+  res.write("</html>");
+  res.end(); // You cant change res once response id sent.
+};
+
+// Ways to export module
+// module.exports = requestHandler;
+// module.exports = {
+//     handler: requestHandler,
+//     someOtherData: "Other Data"
+// };
+// module.exports.handler = requestHandler;
+exports.handler = requestHandler;
+exports.someOtherData = "Other data";
